3ec527e200620e21d5365af2fa0230c46590e98a
pkreborn06 <honey.moneykhandelwal@gmail.com>
2020-02-11 00:26:04 +0530

    Updated PurifyIt.java

--- CodeChef/PurifyIt.java
+++ CodeChef/PurifyIt.java
@@ -1,26 +1,130 @@
+/*
+https://www.codechef.com/COOK113B/problems/PRFYIT
+*/
 import java.util.Scanner;
+import java.util.List;
+import java.util.ArrayList;
+import java.util.Arrays;
 
 public class PurifyIt{
 
-	public static String inputString="";
-	public static Scanner sc;
-	public static int count=0;
-
-	public static ONE_COUNT = oneCount();
-	public static ZERO_COUNT = zeroCount();
+	public static String inputString;
+	public static int count;
+		
 		
 	public static void main(String arg[]){
-		sc = new Scanner(System.in);
+		Scanner sc = new Scanner(System.in);
 		if(sc.hasNextInt()){
 			int T = sc.nextInt();
 			sc.nextLine();
 			while(T>0){
 				inputString = sc.nextLine();
+				count=0;
 				validator();
 				System.out.println(count);
 				T--;
 			}
 		}
+	}
+
+	public static void validator(){
+		while(true){
+			if(traverse()){
+				break;
+			}
+		}
+	}
+
+	public static boolean traverse(){
+
+		int[] max = new int[4];
+		int[] start = new int[4];
+
+		// initialize max array;
+		for(int i=0;i<4;i++){
+			max[i] = 0;
+		}
+
+		//initialize start array
+		for(int i=0;i<4;i++){
+			start[i] = 0;
+		}
+
+		String subSequence = "";
+		char[] inputChar = inputString.toCharArray();
+		
+		int j=0;
+
+		// iterating all over the string to fetch both the patterns
+		for(int i=0;i<inputString.length();i++){
+			if(j<4){
+				if(i!=0){
+					if(inputChar[i]!=inputChar[i-1]){
+						if(j+1==4)break;
+						start[++j] = i;
+						max[j] = 1;
+						subSequence+=String.valueOf(inputChar[i]);
+					}else{
+						if(start[j]==0 && max[j]==0){
+							start[j] = i;
+						}
+						++max[j];
+					}
+				}else{
+					++max[i];
+					subSequence+=String.valueOf(inputChar[i]);
+				}
+			}else{
+				// System.out.println("Exiting string check loop...");
+				break;
+			}
+		}
+
+
+		// if any sequence found
+		if(subSequence.equals("0101") || subSequence.equals("1010")){
+			
+			String[] eliminator = findMinAndStarting(max,subSequence).split(" ");
+			
+			// eliminate the minimum occurance of the number
+			int startLoop = start[Integer.parseInt(eliminator[0])];
+			int endLoop = Integer.parseInt(eliminator[1]);
+
+			List<String> inputAsList = new ArrayList<>();
+
+			inputAsList.addAll(Arrays.asList(inputString.split("")));
+			
+
+			for(int i=startLoop;i<=(startLoop+endLoop-1);i++){
+				inputAsList.remove(i);
+				count++;
+			}
+			
+			inputString = String.join("",inputAsList);
+			return false;
+		}
+		return true;
+	}
+
+	public static String findMinAndStarting(int[] max,String subSequence){
+
+		char skipValue = '0';
+		
+		if(oneCount()>zeroCount()){
+			skipValue = '1';
+		}
+
+		char[] subSequenceChar = subSequence.toCharArray();
+
+		int min = inputString.length();
+		int index = 0;
+		for(int i=0;i<4;i++){
+			if(max[i]<min && subSequenceChar[i]!=skipValue){
+				min = max[i];
+				index = i;
+			}
+		}
+		return index + " " + min;
 	}
 
 	public static int zeroCount(){
@@ -29,34 +133,5 @@ public class PurifyIt{
 
 	public static int oneCount(){
 		return inputString.length() - inputString.replace("1","").length();
-	}
-
-	public static void validator(){
-
-		if(ZERO_COUNT>ONE_COUNT){
-			// operation on 1
-			if(ONE_COUNT>1){
-				purifier(1);
-			}
-		}else{
-			//operation on 0
-			if(ZERO_COUNT){
-				purifier(0)
-			}
-		}
-		
-	}
-
-	public static void purifier(int value){
-			System.out.println("String processed = "  +inputString);
-			int startIndex = inputString.indexOf(SQ_1);
-			System.out.println("startIndex = " + startIndex);
-			if(startIndex!=-1){
-				inputString = inputString.replaceFirst(SQ_1,SQ_1_REPLACE);
-				System.out.println("String replaced SQ1 = "  +inputString);
-				count++;
-				purifyIt();
-			}
-		
 	}
 }
